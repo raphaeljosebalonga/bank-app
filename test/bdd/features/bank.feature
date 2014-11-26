b680efffca0cf443d612f20d8a813b9af9d13f29
raphaeljosebalonga <raphaeljose.balonga@gmail.com>
2014-11-26 18:28:04 +0800

    [Update] Refactored and added more Scenarios for lettuce

--- test/bdd/features/bank.feature
+++ test/bdd/features/bank.feature
@@ -3,8 +3,31 @@ As a customer I wish to be able to view my balance
 and update my balance
 and withdraw from my balance
 
+@1stTest
 Scenario: Retrieve customer balance
 Given I create account "1111" with balance of "50"
 And I visit the homepage
 When I enter the account number "1111"
 Then I see a balance of "50"
+
+@2ndTest
+Scenario Outline: Retrieve customer balance
+Given I create the following account:
+| account_number | balance |
+| 1111           | 50      |
+And I visit the homepage
+When I enter the account number "1111"
+Then I see a balance of "50"
+
+@3rdTest
+Scenario Outline: Retrieve customer balance
+Given I create account "<account_number>" with balance of "<balance>"
+And I visit the homepage
+When I enter the account number "<account_number>"
+Then I see a balance of "<balance>"
+Examples:
+    |account_number|balance|
+    |1111          |50     |
+    |2222          |100    |
+    |3333          |500    |
+    |4444          |1000   |
--- test/bdd/steps/steps.py
+++ test/bdd/steps/steps.py
@@ -10,8 +10,8 @@ from bank_app import app, BANK
 from bank.account import Account
 
 @step(u'Given I create account "([^"]*)" with balance of "([^"]*)"')
-def given_i_create_account_group1_with_balance_of_group2(step, group1, group2):
-    a = Account(group1, group2)
+def given_i_create_account_group1_with_balance_of_group2(step, account_number, balance):
+    a = Account(account_number, balance)
     BANK.add_account(a)
 
 @step(u'And I visit the homepage')
@@ -30,5 +30,11 @@ def when_i_enter_the_account_number_group1(step, account_number):
 
 
 @step(u'Then I see a balance of "([^"]*)"')
-def then_i_see_a_balance_of_group1(step, group1):
-    assert_in("Balance: {}".format(group1), world.form_response.text)
+def then_i_see_a_balance_of_group1(step, balance):
+    assert_in("Balance: {}".format(balance), world.form_response.text)
+
+@step(u'Given I create the following account:')
+def given_i_create_the_following_account(step):
+    for row in step.hashes:
+        a = Account(row['account_number'], row['balance'])
+        BANK.add_account(a)
