bf94f14eb09aa1b53a2a88ffb9da5ca0796e3fe6
raphaeljosebalonga <raphaeljose.balonga@gmail.com>
2014-12-14 19:53:51 +0800

    Coverage Test 3

--- bank/bank.py
+++ bank/bank.py
@@ -7,8 +7,7 @@ class Bank(object):
 
     def get_account_balance(self, account_number): #pragma: no cover
         """ Getter Method """
-        balance = self.accounts.get(account_number)
-        return balance
+        return self.accounts.get(account_number)
 
     def add_account(self, account):
         """ Setter Method """
